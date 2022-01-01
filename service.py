d0d6f0d207049f39d0c6adae9f58ccc5abccee3d
Marc Tran Du Trieu <marc.tdt@outlook.com>
2022-01-01 13:58:53 +0800

    Fix files format from CRLF to LF(issue with darknet) Fix output from detection

--- service.py
+++ service.py
@@ -1,48 +1,53 @@
-
-
 import urllib.request
 import os
 import subprocess
 import boto3
-
+import os.path
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
 
-def downloadFromS3(strBucket,strKey,strFile):
+
+def downloadFromS3(strBucket, strKey, strFile):
     s3_client = boto3.client('s3')
     s3_client.download_file(strBucket, strKey, strFile)
-
 
 
 def handler(event, context):
     try:
         imgfilepath = '/tmp/inputimage.jpg'
-        if ('imagelink' in event):
-          urllib.request.urlretrieve(event['imagelink'], imgfilepath)
-        else:
-          strBucket = 'myqueuecounter'
-          strKey = 'darknet/street.jpg'
-          downloadFromS3(strBucket,strKey,imgfilepath)
-        print(imgfilepath)
-        
-        strBucket = 'myqueuecounter'
-        strKey = 'darknet/yolov3.weights'
+
+        print(os.path.isfile('/tmp/yolov3.weights'))
         strWeightFile = '/tmp/yolov3.weights'
-        downloadFromS3(strBucket,strKey,strWeightFile)  
+        strKey = 'darknet/yolov3.weights'
+        strBucket = 'myqueuecounter'
+        if not os.path.isfile('/tmp/yolov3.weights'):
+            downloadFromS3(strBucket, strKey, strWeightFile)
         print(strWeightFile)
 
-        command = './darknet detect cfg/yolov3.cfg {} {}'.format(
+        if ('imagelink' in event):
+            urllib.request.urlretrieve(event['imagelink'], imgfilepath)
+            print(imgfilepath)
+        else:
+            strBucket = 'myqueuecounter'
+            strKey = 'darknet/street.jpg'
+            downloadFromS3(strBucket, strKey, imgfilepath)
+        print(imgfilepath)
+
+        command = './darknet detect cfg/yolov3.cfg {} {} > /tmp/results.txt'.format(
+            # command='./darknet classifier predict cfg/coco.data cfg/yolov3.cfg {} {} > /tmp/results.txt'.format(
             strWeightFile,
             imgfilepath
         )
+
         print(command)
+        output = None
 
         try:
             print('Start')
             output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
             print('Finish')
-            print(output)
+            # print(output)
         except subprocess.CalledProcessError as e:
             print('Error')
             print((e.output))
@@ -50,4 +55,8 @@ def handler(event, context):
         print('Error e')
         print(e)
         raise e
-    return 0 
+    results_file = open("/tmp/results.txt", "r")
+    results = results_file.readlines()
+    results_file.close()
+    print(results)
+    return results
