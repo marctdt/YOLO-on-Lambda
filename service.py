204c3acc06bd3909c8ffd9225e6aa29e88d2636b
Marc Tran Du Trieu <marc.tdt@outlook.com>
2022-01-06 12:05:25 +0800

    Improve integration with Luna
        Improve integration with Lambda
        Upload the prediction.png to s3
        return json instead of the standard output
        Update to yolov4
        Get the weight file from custom s3

--- service.py
+++ service.py
@@ -12,32 +12,48 @@ def downloadFromS3(strBucket, strKey, strFile):
     s3_client = boto3.client('s3')
     s3_client.download_file(strBucket, strKey, strFile)
 
+def uploadToS3(strBucket, strLocalPath, strRemotePath):
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(strLocalPath, strBucket, strRemotePath)
+
 
 def handler(event, context):
     try:
-        imgfilepath = '/tmp/inputimage.jpg'
+        mediafilepath = '/tmp/inputmedia.jpg'
+        resultFilePath = '/tmp/results.json'
+        predictionFilePath = '/tmp/predictions.png'
 
-        print(os.path.isfile('/tmp/yolov3.weights'))
-        strWeightFile = '/tmp/yolov3.weights'
-        strKey = 'darknet/yolov3.weights'
-        strBucket = 'myqueuecounter'
-        if not os.path.isfile('/tmp/yolov3.weights'):
+        # commandStr = './darknet detect cfg/yolov3.cfg {} {} > /tmp/results.txt'
+        commandStr = './darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg {} {} -out {} -i 0 -thresh 0.25'
+
+        if ('mediatype' in event):
+            if (event['mediatype'].lower() == 'videonote'):
+                mediafilepath = '/tmp/inputmedia.mp4'
+                commandStr = './darknet detector demo cfg/coco.data cfg/yolov4.cfg {} {} -i 0 -thresh 0.25 > {}'
+
+        strWeightFile = '/tmp/yolov.weights'
+        print(os.path.isfile(strWeightFile))
+        # strKey = 'darknet/yolov3.weights'
+        # strBucket = 'myqueuecounter'
+        strKey = 'LunaYOLO/yolov4.weights'
+        strBucket = 'lunappmbucket'
+        if not os.path.isfile(strWeightFile):
             downloadFromS3(strBucket, strKey, strWeightFile)
         print(strWeightFile)
 
         if ('imagelink' in event):
-            urllib.request.urlretrieve(event['imagelink'], imgfilepath)
-            print(imgfilepath)
+            urllib.request.urlretrieve(event['imagelink'], mediafilepath)
         else:
             strBucket = 'myqueuecounter'
             strKey = 'darknet/street.jpg'
-            downloadFromS3(strBucket, strKey, imgfilepath)
-        print(imgfilepath)
+            downloadFromS3(strBucket, strKey, mediafilepath)
+        print(mediafilepath)
 
-        command = './darknet detect cfg/yolov3.cfg {} {} > /tmp/results.txt'.format(
+        command = commandStr.format(
             # command='./darknet classifier predict cfg/coco.data cfg/yolov3.cfg {} {} > /tmp/results.txt'.format(
             strWeightFile,
-            imgfilepath
+            mediafilepath,
+            resultFilePath
         )
 
         print(command)
@@ -47,7 +63,15 @@ def handler(event, context):
             print('Start')
             output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
             print('Finish')
-            # print(output)
+            print(output)
+
+            print('Is predictions file is created: ')
+            print(os.path.isfile(predictionFilePath))
+
+            print('Upload File to S3')
+            strBucketPrediction = 'lunappmbucket'
+            uploadToS3(strBucketPrediction,predictionFilePath,"predictions.png")
+            print('Upload Done')
         except subprocess.CalledProcessError as e:
             print('Error')
             print((e.output))
@@ -55,8 +79,11 @@ def handler(event, context):
         print('Error e')
         print(e)
         raise e
-    results_file = open("/tmp/results.txt", "r")
-    results = results_file.readlines()
+    results_file = open(resultFilePath, "r")
+    # results = results_file.readlines()
+    results = results_file.read()
     results_file.close()
+
+
     print(results)
     return results
